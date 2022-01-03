d6cfb86ffb39d3ecae81ef4705e44ce5cc41eb92
Jc Ayala <j.ayala.higuera@gmail.com>
2022-01-03 19:54:36 -0300

    fix(dev): termino de primeros 3 componentes. Avances en Chromatic

--- .storybook/main.js
+++ .storybook/main.js
@@ -10,9 +10,18 @@ module.exports = {
         },
       },
     },
+    '@storybook/addon-links',
+    '@storybook/addon-docs',
   ],
   // uncomment the property below if you want to apply some webpack config globally
   // webpackFinal: async (config, { configType }) => {
+  //   console.log(configType);
+  //   if (!isReactScriptsInstalled()) {
+  //     logger.info(
+  //       '=> Using base config because react-scripts is not installed.'
+  //     );
+  //     return config;
+  //   }
   //   // Make whatever fine-grained changes you need that should apply to all storybook configs
 
   //   // Return the altered config
